0444b7c562947b810be87db408628a48f6c5e2c9
5harka <92636337+5harka@users.noreply.github.com>
2024-11-14 11:44:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from streamlit import switch_page, session_state
 from streamlit_option_menu import option_menu
 
 from menu import menu_sidebar
-
 st.set_page_config(page_title='Resume Maker', page_icon='📃', initial_sidebar_state='collapsed')
-
-# Logo
+#Logo
 st.logo("https://www.dorsch.ae/fileadmin/Logos/global/Logo_DorschGlobal_1000_w.svg")
+# # Initialize session state for login
+# if 'logged_in' not in st.session_state:
+#     st.session_state.logged_in = False
 
 try:
     with SessionLocal() as db:
@@ -24,14 +25,18 @@ try:
     for index in range(len(emails)):
         credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
 
+    names = [credentials['usernames'][username]['name'] for username in usernames]
+
     Authenticator = stauth.Authenticate(
-        credentials=credentials,
+        names=names,
+        usernames=usernames,
+        passwords=passwords,
         cookie_name='Streamlit',
         key='abcdef',
         cookie_expiry_days=4
     )
 
-    email, authentication_status, username = Authenticator.login('Log In', 'main')
+    email, authentication_status, username = Authenticator.login(':green[Log In]', 'main')
 
     info, info1 = st.columns(2)
 
@@ -41,10 +46,15 @@ try:
     if username:
         if username in usernames:
             if authentication_status:
+
+                # st.session_state.logged_in = True
                 menu_sidebar()
                 st.logo("https://www.dorsch.ae/fileadmin/Logos/global/Logo_DorschGlobal_1000_w.svg")
                 st.sidebar.subheader(f'Welcome :green[{username}]')
                 Authenticator.logout('Log Out', 'sidebar')
+
+
+
             elif not authentication_status:
                 with info:
                     st.error('Incorrect Password or username')
@@ -58,3 +68,4 @@ try:
 except Exception as e:
     st.success('Refresh Page')
     st.error(f'Error: {e}')
+
